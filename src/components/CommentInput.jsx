6bc4f6c2d76f26d818d68caa4e6814c30c7b2603
Pete Laptop <pmugasa78@gmail.com>
2023-07-20 08:31:34 +0200

    changed the input for comments to a textarea

--- src/components/CommentInput.jsx
+++ src/components/CommentInput.jsx
@@ -5,13 +5,13 @@ function CommentInput({ currentUser, addComment, setComment, comment }) {
     <>
       <div className="h-fit w-full border-2 border-gray-100 shadow-sm bg-white mt-4 rounded-lg p-4">
         <form onSubmit={addComment}>
-          <input
+          <textarea
             onChange={(e) => setComment(e.target.value)}
             value={comment}
             type="text"
             placeholder="Add comment..."
-            className="border-2 border-gray-200 h-28 w-full rounded-lg p-4 font-rubik text-gray-400 focus:outline-purple"
-          />
+            className="border-2 border-gray-200 h-28 w-full rounded-lg p-4 font-rubik text-gray-400 focus:outline-purple resize-none"
+          ></textarea>
 
           <div className="flex items-center mt-4">
             <img src={currentUser.image.webp} className="h-8 w-8" />
